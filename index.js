05f227231e640877897f4dc1a28f9b35b219d525
Rahul Golwalkar <rahulgolwalkar@gmail.com>
2021-07-15 21:47:32 -0400

    Commented out the recordDelimeter param

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const data = fs.readFileSync('./win2.csv', 'utf8')
 console.log(data)
 
 let aaa = csvParser.parse(data, {
-  // recordDelimiter: ['\r\n', '\n', '\r', '\n\r']
+  // recordDelimiter: ['\n\r', '\r\n', '\n', '\r'] // uncommenting this line will get it to work - one more issue is that if the array is in a different order (say ['\n', '\r', '\n\r', '\r\n'])  - this will throw an error as well
 }, function (err, parsedArr) {
   console.log(err)
   console.log(parsedArr)
